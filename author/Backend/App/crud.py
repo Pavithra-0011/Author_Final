6715298276e1d087f0d5e79294727716b78592f3
Pavithra <pavithraanshaya@gmail.com>
2025-08-25 02:35:44 +0530

    xc

--- author/Backend/App/crud.py
+++ author/Backend/App/crud.py
@@ -1,43 +1,41 @@
-from App.database import books_collection, authors_collection
+from App.database import db
+from App.models.author import Author
+from App.models.book import Book
 
 #----------BOOKS-------------
 def create_book(book: dict):
-    result = books_collection.insert_one(book)
-    book["_id"] = str(result.inserted_id)
-    return book
+    result = db.Book.insert_one(book)
+    return str(result.inserted_id)
 
 def get_books():
     """Return all books"""
-    books = list(books_collection.find())
+    books = list(db.Book.find())
     for book in books:
         book['_id'] = str(book['_id'])
     return books
 
 def get_book(book_id: str):
     """Return a single book by ID"""
-    from bson.objectid import ObjectId
-    book = books_collection.find_one({"_id": ObjectId(book_id)})
+    book = db.Book.find_one({"_id": book_id})
     if book:
         book['_id'] = str(book['_id'])
     return book
 
 #----------AUTHORS-------------
 def create_author(author: dict):
-    result = authors_collection.insert_one(author)
-    author["_id"] = str(result.inserted_id)
-    return author
+    result = db.Author.insert_one(author)
+    return str(result.inserted_id)
 
 def get_authors():
     """Return all authors"""
-    authors = list(authors_collection.find())
+    authors = list(db.Author.find())
     for author in authors:
         author['_id'] = str(author['_id'])
     return authors
 
 def get_author(author_id: str):
     """Return a single author by ID"""
-    from bson.objectid import ObjectId
-    author = authors_collection.find_one({"_id": ObjectId(author_id)})
+    author = db.Author.find_one({"_id": author_id})
     if author:
         author['_id'] = str(author['_id'])
     return author
